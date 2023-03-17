eda8885899b24d77375561a708ec2ca1482802b8
Alancittoo <echeniquealan@yahoo.com>
2023-03-17 16:40:10 -0400

    updated migration files and readMe

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -10,6 +10,19 @@ module.exports = (sequelize, DataTypes) => {
 
   User.init(
     {
+      id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName:{
+        type: DataTypes.STRING,
+        allowNull: false
+      },
       username: {
         type: DataTypes.STRING,
         allowNull: false,
